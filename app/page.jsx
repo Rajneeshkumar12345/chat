ad7c3dbe9fa7015d7b9c6752c632b33b3e6bf7f4
rajneesh <rajneesh@hexabells.com>
2024-03-07 17:12:25 +0530

    front-and-back

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import CHAT from "../app/chat/page"
+
 
 
 export default function Home() {
 
   return (
     <>
-      <CHAT />
+      <h1>Hiiiii</h1>
     </>
   );
 }
