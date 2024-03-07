4c52065441f503e383dc24aa2ecc1879e2bd0743
rajneesh <rajneesh@hexabells.com>
2024-03-07 18:09:13 +0530

    update-forget

---
+++ app/logins/_components/Otp.js
@@ -0,0 +1,10 @@
+import react from "react";
+
+const Otp = () => {
+    return (
+        <>
+            <h1>Hii otp</h1>
+        </>
+    )
+}
+export default Otp;
---
+++ app/logins/_components/Recovery.js
@@ -0,0 +1,10 @@
+import react from "react";
+
+const Recovery = () => {
+    return (
+        <>
+            <h1>Hii otp</h1>
+        </>
+    )
+}
+export default Recovery;
